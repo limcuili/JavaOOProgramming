88a1ae0f7061f0e872d45834bbdf566ea7b6059b
limcuili <limcuili@hotmail.com>
2018-11-23 11:53:34 +0000

    Had to fixed some careless typos, also contains method

--- Interval.java
+++ Interval.java
@@ -1,9 +1,11 @@
 public class Interval {
-    private double left. right; // instance variables
+    private double left, right; // instance variables
 
-    public Interval(double left; double right) {
+    public Interval(double left, double right) {
         this.left = left;
         this.right = right;
     }
+
+    public boolean contains(double x) { return (left <= x && x <= right); }
 
 }
